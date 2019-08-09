9f944a8c71cbf48813a4a9648cdaf94f2bccc3de
Anifatul Aufah <anifatulaufah09@gmail.com>
2019-08-09 16:48:31 +0700

    looping

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const task_condition = require("./lib/task_condition")
+const loop_for = require('./lib/loop/loop-for')
+const loop_foreach = require('./lib/loop/loop-foreach')
+const loop_map = require('./lib/loop/loop-map')
+const map_object = require('./lib/loop/loop_map_object')
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -24,7 +28,51 @@ app.post("/task2", (req, res) => {
 
     return res.send(result)
 })
+app.get("/loop-for", (req, res) => {
+    // let data = ["Red", "Blue", "Green"]
 
+    let data = {
+        warna1: "Red",
+        warna2: "Blue",
+        warna3: "Green"
+    }
+    let result = loop_for(data)
+    return res.send(result)
+})
+
+app.get("/loop-foreach", (req, res) => {
+    let data = ["Red", "Blue", "Green"]
+    let result = loop_foreach(data)
+
+    return res.send(result)
+})
+
+app.get("/loop-map", (req, res) => {
+    let data = ["Red", "Blue", "Green"]
+    let result = loop_map(data)
+
+    return res.send(result)
+})
+app.get('/object-loop', (req, res) => {
+    let data = [
+        {
+            name: "Red",
+            note: "Danger"
+        },
+        {
+            name: "Yellow",
+            note: "Warning"
+        },
+        {
+            name: "Green",
+            note: "Success"
+        }
+    ]
+
+    let result = map_object(data)
+
+    return res.send(result)
+})
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
 })
