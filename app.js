9494fdcfd572f7ac003decb88a48d63971053a35
Anifatul Aufah <anifatulaufah09@gmail.com>
2019-08-09 12:50:47 +0700

    task condition

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const task_condition = require("./lib/task_condition")
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    return res.send(`Welcome to the beginning of nothingness`)
+})
+
+app.get("/task1", (req, res) => {
+    let key1 = req.query.key1
+    let key2 = req.query.key2
+
+    let result = task_condition.task1(key1, key2)
+
+    return res.send(result)
+})
+
+app.post("/task2", (req, res) => {
+    let { nama, email } = req.body
+    let result = task_condition.task2(nama, email)
+
+    return res.send(result)
+})
+
+app.listen(3300, () => {
+    console.log(`Example app listening on port 3300`)
+})
