d3a20f7c7b14b00366ffc35b38bc005722acdffa
anifatulaufah <anifatulaufah09@gmail.com>
2019-08-21 11:40:46 +0700

    email service

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const { create, getAll, update, destroy, getDetail } = require("../actions/users/register.action")
+const { create, getAll, getDetail, update, destroy } = require("../actions/users")
+const { check, validationResult, body } = require("express-validator")
 
-router.post("/", async (req, res) => {
+router.post("/", [
+    check('name').not().isEmpty(),
+    check('email').not().isEmpty(),
+    check('password').not().isEmpty().isLength({ min: 8 }),
+    check('password_confirmation').not().isEmpty(),
+    body('password_confirmation').custom((value, { req }) => {
+        if(value != req.body.password) {
+            throw new Error('Password confirmation does not match')
+        } else {
+            return true
+        }
+    })
+], async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            status: "error",
+            message: errors.array()
+        })
+    }
+
     try {
         let data = await create(req)
 
@@ -60,7 +82,6 @@ router.put("/:id", async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        password: req.body.password,
         fresh: req.body.fresh
     }
 
